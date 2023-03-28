9185f0c05cffc1d4600d6f928ea4cefa203bc17f
Mingyu Cha <mk.cha2002@gmail.com>
2023-03-28 19:02:28 +0900

    EndSpringPrologue commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,16 +1,19 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 설정파일을 만들기 위한 애노테이션 or Bean을 등록하기 위한 애노테이션이다. 쉽게 말해서, Bean 을 등록하겠다는 뜻
 public class AppConfig {
 
+    @Bean // DI 컨테이너가 관리하는 자바 객체
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입 / control + alt + m(인터페이스의 역할들을 한 눈에 볼 수 있게 된다.)
         // MemoryMemberRepository 객체(인스턴스)를 먼저 생성하여 MemberServiceImpl 을 생성할 때 생성자 매개 변수(파라미터)로 넘김
@@ -18,14 +21,17 @@ public class AppConfig {
         // 주입해주는 것이라고 이해할 수 있다.
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,22 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //      MemberService memberService = new MemberServiceImpl();
+
+
+        // ApplicationContext = 스프링 컨테이너 ,
+        // AnnotationConfigApplicationContext = 자바 어노테이션을 이용한 클래스로부터 객체 설정 정보를 가져온다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // 스프링 컨테이너 생성
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // 자바 언어에서는 인터페이스도 빌드가 되면, .class 가 되기 때문에, 인터페이스와 클래스 구분없이 .class 로 사용 가능하다.
+
         Member member = new Member(1L, "memberA", Grade.VIP); // control + alt + v
         memberService.join(member);
 
